1c3f9870c662a3cd489b7747abd7896fbdb2ee71
Alendroide <alendroideyt@gmail.com>
2025-01-05 16:58:38 -0500

    JWT verification middleware added

--- src/routers/test.router.js
+++ src/routers/test.router.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import testController from '../controllers/test.controller.js';
+import verifyJWT from '../middlewares/verifyJWT.middleware.js';
 
 const router = Router();
 
-router.get('/test',testController);
+router.get('/test',verifyJWT,testController);
 
 export default router
