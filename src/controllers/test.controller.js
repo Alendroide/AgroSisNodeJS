cd1b39bcc9ac2d68e3832ec121304c49997e7063
Alendroide <alendroideyt@gmail.com>
2025-01-07 21:14:10 -0500

    Connection pool added

--- src/controllers/test.controller.js
+++ src/controllers/test.controller.js
@@ -1,5 +1,14 @@
-const test = (req,res) => {
-    res.status(200).json({msg:"Test exitoso"});
+import pool from "../db.js";
+
+const test = async(req,res) => {
+    try{
+        const [tables] = await pool.query("SHOW TABLES");
+        res.status(200).json(tables);
+    }
+    catch(error){
+        res.status(500).json({msg:"Internal server error"});
+        console.error(error);
+    }
 };
 
 export default test;
