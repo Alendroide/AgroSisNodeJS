2caf108517d016bf55a23bb228e00f7e180c4d4f
MaiberCordoba <cordobamaiber@gmail.com>
2025-01-29 16:52:54 -0500

    creando la ruta para la solicitud de eliminar afecciones

--- src/routers/afecciones.router.js
+++ src/routers/afecciones.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   actualizarAfecciones,
+  eliminarAfecciones,
   listarAfecciones,
   registrarAfecciones,
 } from "../controllers/afecciones.controller.js";
@@ -9,5 +10,6 @@ const rutaAfecciones = Router();
 rutaAfecciones.get("/afecciones", listarAfecciones);
 rutaAfecciones.post("/afecciones", registrarAfecciones);
 rutaAfecciones.put("/afecciones/:id", actualizarAfecciones);
+rutaAfecciones.delete("/afecciones/:id", eliminarAfecciones);
 
 export default rutaAfecciones;
