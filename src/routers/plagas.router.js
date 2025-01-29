a5ece1156f0f29ae5dbdad9345c474867fa51a29
MaiberCordoba <cordobamaiber@gmail.com>
2025-01-29 15:45:32 -0500

    arreglando la ruta para poder actualizar por id

--- src/routers/plagas.router.js
+++ src/routers/plagas.router.js
@@ -9,6 +9,6 @@ const rutaPlagas = Router();
 
 rutaPlagas.get("/plagas", listarPlagas);
 rutaPlagas.post("/plagas", registrarPlagas);
-rutaPlagas.put("/plagas", actualizarPlagas);
+rutaPlagas.put("/plagas/:id", actualizarPlagas);
 
 export default rutaPlagas;
