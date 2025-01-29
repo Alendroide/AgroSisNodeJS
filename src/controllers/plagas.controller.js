f8200894d5c15066f32288b07102a0759d845d4b
MaiberCordoba <cordobamaiber@gmail.com>
2025-01-29 15:15:18 -0500

    creaando el controlador que realiza la solicitud para listar las plagas

---
+++ src/controllers/plagas.controller.js
@@ -0,0 +1,15 @@
+import pool from "../db.js";
+
+export const listarPlaga = async (req, resp) => {
+  try {
+    const [result] = await pool.query("select * from plagas");
+    if (result.length > 0) {
+      return resp.status(200).json(result);
+    } else {
+      return resp.status(404).json({ message: "plaga no encontrados" });
+    }
+  } catch (error) {
+    console.error(error);
+    return resp.status(500).json({ message: "error en el sistema" });
+  }
+};
