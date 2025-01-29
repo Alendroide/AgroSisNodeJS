09994476499ca074cfd34d8abeb2ec560eb64ef4
MaiberCordoba <cordobamaiber@gmail.com>
2025-01-29 14:10:50 -0500

    creando el controlador para la solicitud de buscar tipos de planta por su id

--- src/controllers/tipoPlaga.controller.js
+++ src/controllers/tipoPlaga.controller.js
@@ -72,3 +72,22 @@ export const eliminarTipoPlaga = async (req, resp) => {
     return resp.status(500).json({ message: "error en el sistema" });
   }
 };
+
+export const buscarTipoPlaga = async (req, resp) => {
+  try {
+    const id = req.params.id;
+    const [result] = await pool.query(
+      `select * from tiposplaga where id=${id}`
+    );
+    if (result.length > 0) {
+      return resp.status(200).json(result);
+    } else {
+      return resp
+        .status(404)
+        .json({ message: "tipos de plaga no encontrados" });
+    }
+  } catch (error) {
+    console.error(error);
+    return resp.status(500).json({ message: "error en el sistema" });
+  }
+};
