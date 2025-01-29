07c8653eaf6babe8bc6d4de4e5a7a9a9d878dbdf
MaiberCordoba <cordobamaiber@gmail.com>
2025-01-29 16:22:23 -0500

    creando el controlador para la solicitud listar afecciones

--- src/controllers/afecciones.controller.js
+++ src/controllers/afecciones.controller.js
@@ -1,5 +1,15 @@
 import pool from "../db.js";
 
-rutaAfecciones = pool();
-
-export default rutaAfecciones;
+export const listarAfecciones = async (req, resp) => {
+  try {
+    const [result] = await pool.query("select * from afecciones");
+    if (result.length > 0) {
+      return resp.status(200).json(result);
+    } else {
+      return resp.status(404).json({ message: "afecciones no encontradas" });
+    }
+  } catch (error) {
+    console.error(error);
+    return resp.status(500).json({ message: "error en el sistema" });
+  }
+};
