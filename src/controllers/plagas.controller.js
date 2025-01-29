2887462656d8a33a24f5935088df5d990ff70984
MaiberCordoba <cordobamaiber@gmail.com>
2025-01-29 15:47:38 -0500

    creando el controlador para realizar la socitud de eliminar plagas

--- src/controllers/plagas.controller.js
+++ src/controllers/plagas.controller.js
@@ -61,3 +61,22 @@ export const actualizarPlagas = async (req, resp) => {
     return resp.status(500).json({ message: "error en el sistema" });
   }
 };
+
+export const eliminarPlagas = async (req, resp) => {
+  try {
+    const id = req.params.id;
+    const sql = `delete from plagas where id=${id}`;
+
+    const [rows] = await pool.query(sql);
+    if (rows.affectedRows > 0) {
+      return resp.status(200).json({ message: " plaga eliminada" });
+    } else {
+      return resp
+        .status(400)
+        .json({ message: "no fue posible eliminar la plaga" });
+    }
+  } catch (error) {
+    console.error(error);
+    return resp.status(500).json({ message: "error en el sistema" });
+  }
+};
