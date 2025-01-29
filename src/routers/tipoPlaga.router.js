c7e16b820fcc8d24ae38d2ba3858619d0ef26ad9
MaiberCordoba <cordobamaiber@gmail.com>
2025-01-29 14:02:01 -0500

    creando el endpoint para el metodo delete de tipo plaga

--- src/routers/tipoPlaga.router.js
+++ src/routers/tipoPlaga.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   actualizarTipoPlaga,
+  eliminarTipoPlaga,
   listarTipoPlaga,
   registrarTipoPlaga,
 } from "../controllers/tipoPlaga.controller.js";
@@ -10,4 +11,5 @@ const ruta = Router();
 ruta.get("/tipoPlaga", listarTipoPlaga);
 ruta.post("/tipoPlaga", registrarTipoPlaga);
 ruta.put("/tipoPlaga/:id", actualizarTipoPlaga);
+ruta.delete("/tipoPlaga/:id", eliminarTipoPlaga);
 export default ruta;
