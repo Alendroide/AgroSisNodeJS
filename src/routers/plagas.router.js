bb0094fe82b941bfcbbd8d0fc6d62c44d360636d
MaiberCordoba <cordobamaiber@gmail.com>
2025-01-29 15:21:33 -0500

    creando el endpoint de la solicitud listarPlagas

--- src/routers/plagas.router.js
+++ src/routers/plagas.router.js
@@ -1,3 +1,8 @@
 import { Router } from "express";
+import { listarPlagas } from "../controllers/plagas.controller";
 
 const rutaPlagas = Router();
+
+rutaPlagas.get("/plagas", listarPlagas);
+
+export default rutaPlagas;
