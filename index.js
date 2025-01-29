7fb3185cb56370287ae6a1ed4eac9d34819eea1a
MaiberCordoba <cordobamaiber@gmail.com>
2025-01-29 17:29:43 -0500

    registrando las rutas del crud de tipos de control  en el index

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ import testRouter from "./src/routers/test.router.js";
 import rutaTipoPlaga from "./src/routers/tipoPlaga.router.js";
 import rutaPlagas from "./src/routers/plagas.router.js";
 import rutaAfecciones from "./src/routers/afecciones.router.js";
+import rutaTiposControl from "./src/routers/tiposControl.router.js";
 app.use(testRouter);
 app.use(rutaTipoPlaga);
 app.use(rutaPlagas);
 app.use(rutaAfecciones);
-
+app.use(rutaTiposControl);
 //404
 app.use((req, res) => {
   res.status(404).json({ msg: "404 - Not Found" });
