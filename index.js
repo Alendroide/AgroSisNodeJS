fd2c7010490fe8fed0625f60e4cab686077e3334
MaiberCordoba <cordobamaiber@gmail.com>
2025-01-28 19:02:47 -0500

    agregando el router de tipo plaga al index

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 //Importaciones
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
 
 //Inicializaciones
 const app = express();
 
 //Routers
-import testRouter from './src/routers/test.router.js';
+import testRouter from "./src/routers/test.router.js";
+import rutaTipoPlaga from "./src/routers/tipoPlaga.router.js";
 app.use(testRouter);
+app.use(rutaTipoPlaga);
 
 //404
-app.use((req,res)=>{
-    res.status(404).json({msg:"404 - Not Found"});
+app.use((req, res) => {
+  res.status(404).json({ msg: "404 - Not Found" });
 });
 
 //Arranque del servidor
-app.listen(process.env.PORT,()=>{
-    console.log("Server running on port: ",process.env.PORT);
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port: ", process.env.PORT);
 });
