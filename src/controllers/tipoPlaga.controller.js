69236ce4115ab9defebc34cf23d8b712921002fc
MaiberCordoba <cordobamaiber@gmail.com>
2025-01-29 10:09:34 -0500

    creando la solicitud para registrar tipos de plagas

--- src/controllers/tipoPlaga.controller.js
+++ src/controllers/tipoPlaga.controller.js
@@ -16,4 +16,22 @@ const listarTipoPlaga = async (req, resp) => {
   }
 };
 
+export const registrarTipoPlaga = async (req, resp) => {
+  try {
+    const { nombre, descripcion, img } = req.body;
+    const sql = `insert into tiposplaga (nombre,descripcion,img) values (?,?,?)`;
+    const [result] = await pool.query(sql, [nombre, descripcion, img]);
+    if (result.length > 0) {
+      return resp.status(200).json({ message: "tipo de plaga registrada" });
+    } else {
+      return resp
+        .status(400)
+        .json({ message: "el articulo no se pudo registrar" });
+    }
+  } catch (error) {
+    console.error(error);
+    return resp.status(500).json({ message: "error en el sistema" });
+  }
+};
+
 export default listarTipoPlaga;
