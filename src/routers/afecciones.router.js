58c754c25c3d15693aa2c305b2329650ccd7b5d3
MaiberCordoba <cordobamaiber@gmail.com>
2025-01-29 16:23:18 -0500

    creando la ruta para la solicitud listar afecciones

--- src/routers/afecciones.router.js
+++ src/routers/afecciones.router.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
+import { listarAfecciones } from "../controllers/afecciones.controller";
 
 rutaAfecciones = Router();
+rutaAfecciones.get("/afecciones", listarAfecciones);
 
 export default rutaAfecciones;
