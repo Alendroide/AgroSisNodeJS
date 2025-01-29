3ae8a55c45a5bddb611df8facf11f0d66617b642
MaiberCordoba <cordobamaiber@gmail.com>
2025-01-29 15:41:09 -0500

    arreglando errores en el controlador para registrar plagas

--- src/controllers/plagas.controller.js
+++ src/controllers/plagas.controller.js
@@ -16,8 +16,8 @@ export const listarPlagas = async (req, resp) => {
 
 export const registrarPlagas = async (req, resp) => {
   try {
-    const { nombre, descripcion, img } = req.body;
-    const sql = `insert into plagas (fk_TiposPlaga,nombre,descripcion,img) values (?,?,?)`;
+    const { fk_TiposPlaga, nombre, descripcion, img } = req.body;
+    const sql = `insert into plagas (fk_TiposPlaga,nombre,descripcion,img) values (?,?,?,?)`;
     const [rows] = await pool.query(sql, [
       fk_TiposPlaga,
       nombre,
@@ -30,6 +30,26 @@ export const registrarPlagas = async (req, resp) => {
       return resp
         .status(400)
         .json({ message: " nueva plaga no se pudo registrar" });
+    }
+  } catch (error) {
+    console.error(error);
+    return resp.status(500).json({ message: "error en el sistema" });
+  }
+};
+
+export const actualizarPlagas = async (req, resp) => {
+  try {
+    const id = req.params.id;
+    const { fk_TiposPlaga, nombre, descripcion, img } = req.body;
+    const sql = `update plagas set fk_TiposPlaga=?,nombre=?,descripcion=?,img=? where id=${id}`;
+
+    const [rows] = await pool.query(sql, [nombre, descripcion, img]);
+    if (rows.affectedRows > 0) {
+      return resp.status(200).json({ message: "tipo de plaga actualizada" });
+    } else {
+      return resp
+        .status(400)
+        .json({ message: "no fue posible actualizar el tipo de plaga" });
     }
   } catch (error) {
     console.error(error);
