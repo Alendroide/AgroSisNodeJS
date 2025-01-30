26221e8dcc6410a5e3637ee9c3d0b0dd73417fea
MaiberCordoba <cordobamaiber@gmail.com>
2025-01-30 11:53:18 -0500

    creando el controlador para la solicitud de registrar productos de control

--- src/controllers/productosControl.controller.js
+++ src/controllers/productosControl.controller.js
@@ -15,3 +15,43 @@ export const listarProductosControl = async (req, resp) => {
     return resp.status(500).json({ message: "Error en el sistema" });
   }
 };
+
+export const registrarProductosControl = async (req, resp) => {
+  try {
+    const {
+      nombre,
+      precio,
+      compuestoActivo,
+      fichaTecnica,
+      contenido,
+      tipoContenido,
+      unidades,
+    } = req.body;
+
+    const sql = `INSERT INTO productoscontrol (nombre, precio,compuestoActivo,fichaTecnica,contenido,tipoContenido,unidades)
+                VALUES (?,?,?,?,?,?,?)`;
+
+    const [rows] = await pool.query(sql, [
+      nombre,
+      precio,
+      compuestoActivo,
+      fichaTecnica,
+      contenido,
+      tipoContenido,
+      unidades,
+    ]);
+
+    if (rows.affectedRows > 0) {
+      return resp
+        .status(200)
+        .json({ message: "producto de control registrado" });
+    } else {
+      return resp
+        .status(400)
+        .json({ message: "No se pudo registrar el producto de control" });
+    }
+  } catch (error) {
+    console.error(error);
+    return resp.status(500).json({ message: "Error en el sistema" });
+  }
+};
