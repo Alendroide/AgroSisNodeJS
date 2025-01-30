5b126d394fd643b1e2ea2aca16c991c4e45cf9d2
MaiberCordoba <cordobamaiber@gmail.com>
2025-01-30 11:43:14 -0500

    eliminando archivo repetido

--- src/routers/productosControl.controller.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { listarProductosControl } from "../controllers/productosControl.controller.js";
-
-const rutaProductosControl = Router();
-rutaProductosControl.get("/productosControl", listarProductosControl);
-
-export default rutaProductosControl;
