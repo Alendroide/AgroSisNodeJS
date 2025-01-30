9f24c322efef2e309c4bcb047d44abb783e7f20c
MaiberCordoba <cordobamaiber@gmail.com>
2025-01-30 12:30:26 -0500

    creando los controladores para las solicitudes put, delete y get por id

--- src/controllers/productosControl.controller.js
+++ src/controllers/productosControl.controller.js
@@ -55,3 +55,84 @@ export const registrarProductosControl = async (req, resp) => {
     return resp.status(500).json({ message: "Error en el sistema" });
   }
 };
+
+export const actualizarProductosControl = async (req, resp) => {
+  try {
+    const id = req.params.id;
+    const {
+      nombre,
+      precio,
+      compuestoActivo,
+      fichaTecnica,
+      contenido,
+      tipoContenido,
+      unidades,
+    } = req.body;
+    const sql = `UPDATE productoscontrol SET nombre=?, precio=?, compuestoActivo=?, fichaTecnica=?, contenido=?, tipoContenido=?, unidades=?  WHERE id=${id}`;
+
+    const [rows] = await pool.query(sql, [
+      nombre,
+      precio,
+      compuestoActivo,
+      fichaTecnica,
+      contenido,
+      tipoContenido,
+      unidades,
+    ]);
+
+    if (rows.affectedRows > 0) {
+      return resp
+        .status(200)
+        .json({ message: "productos de control actualizado" });
+    } else {
+      return resp
+        .status(400)
+        .json({ message: "No se pudo actualizar el producto de control" });
+    }
+  } catch (error) {
+    console.error(error);
+    return resp.status(500).json({ message: "Error en el sistema" });
+  }
+};
+
+export const eliminarProductosControl = async (req, resp) => {
+  try {
+    const id = req.params.id;
+    const sql = `DELETE FROM productoscontrol WHERE id=${id}`;
+
+    const [rows] = await pool.query(sql);
+
+    if (rows.affectedRows > 0) {
+      return resp
+        .status(200)
+        .json({ message: "producto de control ha sido eliminado" });
+    } else {
+      return resp
+        .status(400)
+        .json({ message: "No es posible  eliminar el producto de control" });
+    }
+  } catch (error) {
+    console.error(error);
+    return resp.status(500).json({ message: "Error en el sistema" });
+  }
+};
+
+export const buscarProductosControl = async (req, resp) => {
+  try {
+    const id = req.params.id;
+    const [result] = await pool.query(
+      `SELECT * FROM productoscontrol WHERE id=${id}`
+    );
+
+    if (result.length > 0) {
+      return resp.status(200).json(result);
+    } else {
+      return resp
+        .status(404)
+        .json({ message: "producto de control no encontrado" });
+    }
+  } catch (error) {
+    console.error(error);
+    return resp.status(500).json({ message: "Error en el sistema" });
+  }
+};
