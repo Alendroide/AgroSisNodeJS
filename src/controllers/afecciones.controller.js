fb4dcebbcd4b7926dcbaad63ba6a14235d877bcd
MaiberCordoba <cordobamaiber@gmail.com>
2025-01-31 14:05:02 -0500

    agregando en la consullta sql de listar afecciones, para mostrar datos de la era y lote que presente la afeccion

--- src/controllers/afecciones.controller.js
+++ src/controllers/afecciones.controller.js
@@ -6,13 +6,15 @@ export const listarAfecciones = async (req, resp) => {
     p.nombre as nombre_plaga, p.fk_TiposPlaga as idTipoPlaga, tp.nombre as tipo_plaga,
     pl.id as id_plantaciones,
     c.id as id_cultivo, c.nombre as nombre_cultivo, c.unidades as unCultivo,
-    er.id as id_era, er.posX as posXera, er.posY as posYera
+    er.id as id_era, er.posX as posXera, er.posY as posYera,
+    lo.id as id_lote, lo.posX as posXlote, lo.posY as posYlote 
     from afecciones a
     join plagas p on a.fk_Plagas = p.id
     join tiposPlaga tp on p.fk_TiposPlaga = tp.id
     join plantaciones pl on fk_Plantaciones = pl.id
     join cultivos c on fk_Cultivos = c.id
-    join eras er on fk_Eras = er.id`;
+    join eras er on fk_Eras = er.id
+    join lotes lo on fk_Lotes = lo.id`;
     const [result] = await pool.query(sql);
     if (result.length > 0) {
       const afecciones = result.map((afeccion) => ({
