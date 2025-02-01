160703f501e6f555c0b38e6692783537d2cd72e4
Juniorr-7 <juniorfajardom@gmail.com>
2025-01-31 21:53:13 -0500

    Importacion de rutas al index

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ const app = express();
 
 //Routers
 import testRouter from './src/routers/test.router.js';
+import evapotranspiraciones from './src/routers/evapotranspiraciones.router.js';
+import temperaturas from './src/routers/temperaturas.router.js';
+import iluminaciones from './src/routers/iluminaciones.router.js';
+import precipitaciones from './src/routers/precipitaciones.router.js';
+import velocidadViento from './src/routers/velocidadViento.router.js';
+
 app.use(testRouter);
+app.use(evapotranspiraciones);
+app.use(iluminaciones)
+app.use(precipitaciones)
+app.use(temperaturas)
+app.use(velocidadViento)
 
 //404
 app.use((req,res)=>{
