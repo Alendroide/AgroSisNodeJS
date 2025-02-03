413ec2a9d6e9a0fd5d7356f8dff422a860f8ca55
MaiberCordoba <cordobamaiber@gmail.com>
2025-02-03 15:20:52 -0500

    agregando a la consulta del  controlador de listar por id los usos dados  a los productos en los controles informacion mas detallada de las fk y realizando un mapeo para organizar la informacion.

--- src/controllers/usoProductoControl.controller.js
+++ src/controllers/usoProductoControl.controller.js
@@ -160,12 +160,80 @@ export const eliminarUsoProductoControl = async (req, resp) => {
 export const buscarUsoProductoControl = async (req, resp) => {
   try {
     const id = req.params.id;
-    const [result] = await pool.query(
-      `SELECT * FROM usoproductocontrol WHERE id=${id}`
-    );
+    const sql = `select UPC.id as id_UPC, UPC.cantidadProducto as cantidadProducto_UPC,
+      PC.id as id_PC, PC.nombre as nombre_PC, PC.compuestoActivo, PC.tipoContenido as tipoContenido_PC, PC.unidades as unidades_PC,
+      co.id as id_co, co.descripcion as descripcion_co, co.fechaControl,
+      tpC.id as id_tpC, tpC.nombre as nombre_tpC, tpC.descripcion as descripcion_tpC,
+      a.id, a.fechaEncuentro, a. estado, a.fk_plagas,
+      p.nombre as nombre_plaga, p.fk_TiposPlaga as idTipoPlaga, tp.nombre as tipo_plaga,
+      pl.id as id_plantaciones,
+      c.id as id_cultivo, c.nombre as nombre_cultivo, c.unidades as unCultivo,
+      er.id as id_era, er.posX as posXera, er.posY as posYera,
+      lo.id as id_lote, lo.posX as posXlote, lo.posY as posYlote 
+    from usoproductocontrol UPC
+    join productoscontrol PC on fk_ProductosControl = PC.id
+    join controles co on fk_Controles = co.id   
+    join tiposcontrol tpC on fk_TiposControl = tpC.id
+    join afecciones a on fk_Afecciones = a.id
+    join plagas p on a.fk_Plagas = p.id
+    join tiposPlaga tp on p.fk_TiposPlaga = tp.id
+    join plantaciones pl on fk_Plantaciones = pl.id
+    join cultivos c on fk_Cultivos = c.id
+    join eras er on fk_Eras = er.id
+    join lotes lo on fk_Lotes = lo.id
+    where id=${id}`;
+    const [result] = await pool.query(sql);
 
     if (result.length > 0) {
-      return resp.status(200).json(result);
+      const usosPC = result.map((usos) => ({
+        id_usoProductoControl: usos.id_UPC,
+        fk_ProductosControl: {
+          id_productoControl: usos.id_PC,
+          nombre: usos.nombre_PC,
+          compuestoActivo: usos.compuestoActivo,
+          tipoContenido: usos.tipoContenido_PC,
+          unidades: usos.unidades_PC,
+        },
+        cantidad_producto_usada: usos.cantidadProducto_UPC,
+        fk_Controles: {
+          id: usos.id_co,
+          descripcion: usos.descripcion_co,
+          fechaControl: usos.fechaControl,
+          fk_TipoContro: {
+            id: usos.id_tpC,
+            nombre: usos.nombre_tpC,
+            descripcion: usos.descripcion_tpC,
+          },
+          fk_Afecciones: {
+            id: usos.id,
+            fechaEncuentro: usos.fechaEncuentro,
+            estado: usos.estado,
+            fk_Plagas: {
+              idPlaga: usos.id,
+              nombre: usos.nombre_plaga,
+            },
+            fk_Plantaciones: {
+              id: usos.id_plantaciones,
+              fk_cultivo: {
+                id_cultivo: usos.id_cultivo,
+                nombre: usos.nombre_cultivo,
+                unidades: usos.unCultivo,
+              },
+              fk_era: {
+                id: usos.id_era,
+                posX: usos.posXera,
+                posY: usos.posYera,
+                fk_lote: {
+                  id: usos.id_lote,
+                  posX: usos.posXlote,
+                  posY: usos.posYlote,
+                },
+              },
+            },
+          },
+        },
+      }));
+      return resp.status(200).json(usosPC);
     } else {
       return resp.status(404).json({ message: "registro no encotrado" });
     }
