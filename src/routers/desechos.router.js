6acfe9fd84653a3856b57f285adb69745038396b
Mamuel17 <rubyesperanza596@gmail.com>
2025-02-04 21:40:56 -0500

    Controlador y rutas para la tabla desechos

---
+++ src/routers/desechos.router.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { getAllDesechos, createDesechos, updateDesechos } from "../controllers/desechos.controller.js";
+
+const desechos = Router()
+desechos.get("/desechos",getAllDesechos)
+desechos.post("/desechos",createDesechos)
+desechos.put("/desechos",updateDesechos)
+
+export default desechos;
