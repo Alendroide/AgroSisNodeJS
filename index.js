d89497b5ac2abd35a44ddaa94a9706a1388fc6a3
Mamuel17 <rubyesperanza596@gmail.com>
2025-02-04 21:46:45 -0500

    Importacion de las rutas al index

--- index.js
+++ index.js
@@ -8,7 +8,21 @@ const app = express();
 
 //Routers
 import testRouter from './src/routers/test.router.js';
+import cosechas from './src/routers/cosechas.router.js';
+import desechos from './src/routers/desechos.router.js';
+import especies from './src/routers/especies.router.js';
+import semilleros from './src/routers/semilleros.router.js';
+import tiposDesecho from './src/routers/tiposDesecho.router.js';
+import tiposEspecie from './src/routers/tiposEspecie.router.js';
+import ventas from './src/routers/ventas.router.js';
 app.use(testRouter);
+app.use(cosechas)
+app.use(desechos)
+app.use(especies)
+app.use(semilleros)
+app.use(tiposDesecho)
+app.use(tiposEspecie)
+app.use(ventas)
 
 //404
 app.use((req,res)=>{
