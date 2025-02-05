ad00e1a78f3b800cd510b40002b3de3298a79245
Mamuel17 <rubyesperanza596@gmail.com>
2025-02-04 21:45:16 -0500

    Creacion de controlador y rutas para TiposEspecie

---
+++ src/routers/tiposEspecie.router.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { createTiposEspecie, getAllTiposEspecie, updateTiposEspecie } from "../controllers/tiposEspecie.controller.js";
+
+const tiposEspecie = Router()
+tiposEspecie.get("/tiposEspecie",getAllTiposEspecie)
+tiposEspecie.post("/tiposEspecie",createTiposEspecie)
+tiposEspecie.put("/tiposEspecie",updateTiposEspecie)
+
+export default tiposEspecie;
