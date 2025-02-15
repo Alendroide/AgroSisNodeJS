3ab626a701b786ada4861a9705760016507b000f
MaiberCordoba <cordobamaiber@gmail.com>
2025-02-14 22:14:58 -0500

    agregando jwt en el endpoint humedadAmbiente

--- src/routers/humedadAmbiental.router.js
+++ src/routers/humedadAmbiental.router.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { createHumedadAmbiental, getAllHumedadAmbiental } from "../controllers/humedadAmbiental.controller.js";
+import verifyJWT from '../middlewares/verifyJWT.middleware.js';
 
 const humedadAmbiental = Router();
 
-humedadAmbiental.get("/humedadAmbiental",getAllHumedadAmbiental)
-humedadAmbiental.post("/humedadAmbiental",createHumedadAmbiental)
+humedadAmbiental.get("/humedadAmbiental",verifyJWT, getAllHumedadAmbiental)
+humedadAmbiental.post("/humedadAmbiental",verifyJWT, createHumedadAmbiental)
 
 
 
